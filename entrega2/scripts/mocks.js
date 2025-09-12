daf0c31908cd54de32e8efbb15b6c865c6612851
Tomás <tomichimaylov98@gitlab.com>
2025-09-11 23:07:14 -0300

    altaevento y tiporegistro html andando, mocks pruebas

--- entrega2/scripts/mocks.js
+++ entrega2/scripts/mocks.js
@@ -6,6 +6,9 @@ const usuarios = [
   { nick: "maria12", nombre: "María Gómez", correo: "maria@example.com", tipo: "Asistente" }
 ];
 
+// mock.js
+const categorias = ["Música", "Deportes", "Conferencias", "Cine"];
+
 // Eventos de prueba
 const eventos = [
   { id: 1, nombre: "Evento de Música" },
@@ -18,3 +21,6 @@ const ediciones = [
   { eventoId: 1, nombre: "Música 2024", ciudad: "Montevideo", fechaInicio: "2024-05-01", fechaFin: "2024-05-05" },
   { eventoId: 2, nombre: "Ing 2025", ciudad: "Salto", fechaInicio: "2025-01-10", fechaFin: "2025-01-12" }
 ];
+
+
+const tiposRegistro = [];
