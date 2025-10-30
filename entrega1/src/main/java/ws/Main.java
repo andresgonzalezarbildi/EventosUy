f00dadbbe96a901162ebbb9fc346ab09c9e5fb60
maicol-perdomo <maicolperdomocesar@gmail.com>
2025-10-30 20:38:39 -0300

    mini fixs

--- entrega1/src/main/java/ws/Main.java
+++ entrega1/src/main/java/ws/Main.java
@@ -11,22 +11,25 @@ public class Main {
   public static void main(String[] args) {
 
     System.out.println("Iniciando Web Services...");
+    
     EventosWs eventoWs = new EventosWs();
+
     UsuarioWs usuarioWs = new UsuarioWs();
-
-    eventoWs.publicar();
-    usuarioWs.publicar();
-
     
     System.out.println("Web Services publicados");
+    
+    eventoWs.publicar();
     System.out.println("  - EventosWS: http://localhost:9128/Servicios/EventosWS");
+    
+    usuarioWs.publicar();
     System.out.println("  - UsuariosWS: http://localhost:9128/Servicios/UsuariosWS");
-    System.out.println("app swing");
     
     SwingUtilities.invokeLater(()-> {
       Principal frame = new Principal();
       frame.setLocationRelativeTo(null);
       frame.setVisible(true);
     });
+    
+    System.out.println("app swing");
   }
 }
